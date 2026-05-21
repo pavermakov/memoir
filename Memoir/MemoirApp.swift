956c1e110e60d864630336cac00b8377d6db9b21
Pavel Ermakov <pavermakov@gmail.com>
2026-05-21 17:03:17 +0400

    Initial Commit

---
+++ Memoir/MemoirApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoirApp.swift
+//  Memoir
+//
+//  Created by Pavel Ermakov on 21.05.26.
+//
+
+import SwiftUI
+
+@main
+struct MemoirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
